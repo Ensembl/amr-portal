ad3f478a14564b793bed08c187e81c075fe6da73
Bilal <bilal@ebi.ac.uk>
2025-07-22 20:00:06 +0100

    finalize the backend logic

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
-from typing import Union
+from typing import List, Literal, Optional
+from collections import defaultdict
 
 from fastapi import FastAPI
+from fastapi import HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 
 import duckdb
 import numpy as np
 
+from pydantic import BaseModel
 from filters_config import FILTERS_CONFIG
 
 app = FastAPI()
@@ -21,14 +24,65 @@ app.add_middleware(
 # Connecting to the DuckDB database
 data = duckdb.read_parquet("step1_merge_all_v7.parquet")
 
+class SelectedFilter(BaseModel):
+    # the column name
+    category: str
+    # the value to filter by
+    value: str
+
+class OrderBy(BaseModel):
+    category: str
+    order: Literal["ASC", "DESC"]
+
+class Payload(BaseModel):
+    selected_filters: List[SelectedFilter]
+    page: Optional[int] = 1
+    per_page: Optional[int] = 100
+    order_by: Optional[OrderBy] = None
+
 @app.get("/filters-config")
 def get_filters_confi():
     return FILTERS_CONFIG
 
-@app.get("/amr-records")
-def get_amr_records():
-    res_df = data.query("amr_table", "SELECT * FROM amr_table LIMIT 10").fetchdf()
-    # replace all NaN, inf, and -inf with None, which FastAPI will serialize as null
+@app.post("/amr-records")
+def get_amr_records(payload: Payload):
+    grouped_filters = defaultdict(list)
+    for f in payload.selected_filters:
+        grouped_filters[f.category].append(f.value)
+
+    where_clauses = []
+    for category, values in grouped_filters.items():
+        quoted = [f"'{v}'" for v in values]
+        in_clause = f"{category} IN ({', '.join(quoted)})"
+        where_clauses.append(in_clause)
+
+    where_sql = " AND ".join(where_clauses)
+    base_query = "SELECT * FROM amr_table"
+    if where_sql:
+        base_query += f" WHERE {where_sql}"
+
+    # Count query
+    count_query = f"SELECT COUNT(*) AS count FROM amr_table"
+    if where_sql:
+        count_query += f" WHERE {where_sql}"
+
+    total_hits = data.query("amr_table", count_query).fetchone()[0]
+
+    # ORDER BY validation
+    ALLOWED_ORDER_COLUMNS = set(data.columns)
+    if payload.order_by and payload.order_by.category not in ALLOWED_ORDER_COLUMNS:
+        raise HTTPException(status_code=400, detail="Invalid column for ordering")
+
+    # Pagination
+    offset = (payload.page - 1) * payload.per_page
+    paginated_query = base_query
+    if payload.order_by:
+        paginated_query += f" ORDER BY {payload.order_by.category} {payload.order_by.order}"
+    paginated_query += f" LIMIT {payload.per_page} OFFSET {offset}"
+
+    # Final query execution
+    print(f"paginated_query ---> {paginated_query}")
+    res_df = data.query("amr_table", paginated_query).fetchdf()
     res_df = res_df.replace({np.nan: None, np.inf: None, -np.inf: None})
 
     result = []
@@ -45,8 +99,7 @@ def get_amr_records():
             "assembly_accession_id": row.get("Assembly_ID"),
             "phenotype": row.get("phenotype"),
             "measurement": {
-                "value": str(row.get("measurement_value")) if row.get("measurement_value") not in [None,
-                                                                                                   "nan"] else None,
+                "value": str(row.get("measurement_value")) if row.get("measurement_value") not in [None, "nan"] else None,
                 "sign": row.get("measurement_sign"),
                 "unit": row.get("measurement_unit"),
             },
@@ -61,8 +114,12 @@ def get_amr_records():
         }
         result.append(record)
 
-    return result
+    return {
+        "meta": {
+            "total_hits": total_hits,
+            "page": payload.page,
+            "per_page": payload.per_page,
+        },
+        "data": result
+    }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
