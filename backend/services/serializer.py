a05e8ae10436e9fdbecd393463e580f3db6afb98
Jon Keatley <jon@ebi.ac.uk>
2025-09-24 16:43:14 +0100

    Switch serializer to be data driven

--- backend/services/serializer.py
+++ backend/services/serializer.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def serialize_amr_record(row):
+KNOWN_ATTRIBUTES = ["type","sortable", "url", "delimiter"]
+
+def serialize_amr_record(row, column_details:dict):
     def val(key):
         v = row.get(key)
         return None if v in [None, "nan", np.nan, np.inf, -np.inf] else v
@@ -8,35 +10,29 @@ def serialize_amr_record(row):
     result = []
     keys = row.keys()
 
-    skip_fields = {
-        "genus", "species",
-        "measurement_value", "measurement_unit", "measurement_sign"
-    }
-
     for col in keys:
-        if col in skip_fields:
-            continue
-
         v = val(col)
-
-        if col.lower() in {"assembly", "assembly_id"}:
-            result.append({
-                "type": "link",
-                "column_id": "assembly",
-                "value": v,
-                "url": f"https://www.ebi.ac.uk/ena/browser/view/{v}" if v else None
-            })
-        elif col.lower().endswith("_date") or col.lower() == "collection_date":
-            result.append({
-                "type": "string",
-                "column_id": col,
-                "value": str(v) if v is not None else None
-            })
+        
+        if col in column_details:
+            # Add column detail attributes to cell
+            cd = column_details[col]
+            cell_obj = {
+                cd_k:cd_v for cd_k, cd_v in cd.items() 
+                if cd_k in KNOWN_ATTRIBUTES and cd_v is not None
+            }
+            
+            # inject value for url 
+            if "url" in cell_obj:
+                cell_obj["url"] = cell_obj["url"].format(v)
+            # Add missing attribute
+            cell_obj["column_id"] = col
+            cell_obj["value"] = v
+            result.append(cell_obj)
         else:
             result.append({
                 "type": "string",
                 "column_id": col,
                 "value": v
             })
-
+        
     return result
