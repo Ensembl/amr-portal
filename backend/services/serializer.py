8e0b8fe1f94d1362055b62577756a18883e42a25
Jon Keatley <jon@ebi.ac.uk>
2025-10-09 17:22:06 +0100

    Added labelled-link type and supporting backend code

--- backend/services/serializer.py
+++ backend/services/serializer.py
@@ -30,6 +30,14 @@ def serialize_amr_record(row, column_details:dict):
                 else:
                     cell_obj["url"] = None
                 cell_obj["value"] = v
+            elif cell_obj["type"] == "labelled-link":
+                v_bits = v.split('|')
+                cell_obj["type"] = "link" #switch type to url for client
+                if len(v_bits) >= 2:
+                    cell_obj["url"] = v_bits[1]
+                    cell_obj["value"] = v_bits[0]
+                else:
+                    cell_obj["value"] = v;
             elif cell_obj["type"] == "array-link":
                 if set(("delimiter","url")) <=  cell_obj.keys() and v:
                     v_bits = v.split(cell_obj["delimiter"])
