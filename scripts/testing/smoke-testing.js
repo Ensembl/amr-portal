f4d73be85e53cd52ab669510b7ad3aa32a07bca1
Kamal Dodiya <40833799+kamaldodiya@users.noreply.github.com>
2025-10-17 14:56:18 +0100

    K8s and test update (#20)

--- scripts/testing/smoke-testing.js
+++ scripts/testing/smoke-testing.js
@@ -7,7 +7,7 @@ export const options = {
   vus: 1,
 };
 
-let hostName = 'http://amr.review.ensembl.org';
+let hostName = 'http://amr.review.ensembl.org/amr';
 
 async function checkAMRAPI(hostName) {
   let host = '';
@@ -36,9 +36,9 @@ async function checkAMRAPI(hostName) {
   group (`/amr_records`, function () {
   let params = {
       headers: {
-        Connection: `keep-alive`,
-        "content-type": `application/json`,
-        Accept: `*/*`,
+        Connection: "keep-alive",
+        "content-type": "application/json",
+        Accept: "*/*",
       },
       cookies: {},
     };
@@ -48,42 +48,28 @@ async function checkAMRAPI(hostName) {
       amrRecordsEndpoint,
       JSON.stringify({
         "selected_filters": [
-          { "category": "phenotype-genus", "value": "Streptococcus" },
-          { "category": "phenotype-Antibiotic_abbreviation", "value": "OXA" },
-          { "category": "phenotype-Antibiotic_abbreviation", "value": "AMK" }
+          { "category": "phenotype-antibiotic_name", "value": "amikacin"}
         ],
         "view_id": 1,
-        "order_by": {
-          "category": "phenotype-collection_date",
-          "order": "DESC"
-        }
       }),
       params,
     );
 
-    check(resp, { ["AMR records : status equals 200"]: (r) => r.status === 200,
-                  [`has 4 number of hits `] : (r) => r.json().meta["total_hits"] == 4
-                });
+    check(resp, { ["AMR records : status equals 200"]: (r) => r.status === 200});
 
     resp = http.request(
       "POST",
       amrRecordsEndpoint,
       JSON.stringify({
         "selected_filters": [
-          { "category": "genotype-Contig_id", "value": "CQKJ01000001.1" },
-          { "category": "genotype-Contig_id", "value": "DAFBZU010000245.1" }
+          {"category":"genotype-organism_name","value":"Acinetobacter baumannii"}
         ],
-        "view_id": 2,
-        "order_by": {
-          "category": "genotype-Contig_id",
-          "order": "DESC"
-        }
+        "view_id": 2
       }),
       params,
     );
 
     check(resp, { ["AMR genotype data : status equals 200"]: (r) => r.status === 200,
-                  [`has 3 number of hits `] : (r) => r.json().meta["total_hits"] == 3
                 });
 
     resp = http.request(
@@ -91,15 +77,9 @@ async function checkAMRAPI(hostName) {
       amrRecordsEndpoint,
       JSON.stringify({
         "selected_filters": [
-          { "category": "phenotype-genus", "value": "Streptococcus" },
-          { "category": "phenotype-Antibiotic_abbreviation", "value": "OXA" },
-          { "category": "phenotype-Antibiotic_abbreviation", "value": "AMK" }
+            { "category": "phenotype-antibiotic_name", "value": "amikacin"}
         ],
         "view_id": 1,
-        "order_by": {
-          "category": "phenotype-collection_date",
-          "order": "DESC"
-        }
       }),
       params,
     );
