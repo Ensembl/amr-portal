f0f898754a58d64f1d96758090519d375e1df11b
Andrew Yates <andrewyatz@users.noreply.github.com>
2025-11-18 10:46:52 +0000

    Ayates/schema_changes (#49)
    * Fixing cleanup script
    
    * Updated schema
    
    * Updating schema in the files
    
    * Updated configmap to point to latest data release
    
    ---------
    
    Co-authored-by: Jon Keatley <122280653+jonk-ebi@users.noreply.github.com>

--- scripts/cabbage_data_prep/main.py
+++ scripts/cabbage_data_prep/main.py
@@ -2,51 +2,48 @@ import sys
 import re
 import csv
 
-def clean_column_names(columns:[str]) -> [str]:
+
+def clean_column_names(columns: [str]) -> [str]:
     regex = "[a-z][A-Z][a-z]"
     clean_headers = []
     for f in columns:
         n = ""
-        if '_' in f:
+        if "_" in f:
             n = f
-        elif ' ' in f:
-            n = f.replace(' ','_')
+        elif " " in f:
+            n = f.replace(" ", "_")
         else:
-            hits = re.findall(regex,f)
+            hits = re.findall(regex, f)
             if len(hits) == 0:
                 n = f
             else:
                 index = 0
                 for h in hits:
-                    i = f.index(h,index)
-                    n += f[index:i] + h[0] + '_' + h[1].lower() + h[2]
+                    i = f.index(h, index)
+                    n += f[index:i] + h[0] + "_" + h[1].lower() + h[2]
                     index = i + len(h)
                 n += f[index:]
         # remove asterisk
-        n = n.replace('*','')
+        n = n.replace("*", "")
         clean_headers.append(n)
     return clean_headers
 
-def process(input_csv:str, output_csv:str)->bool:
+
+def process(input_csv: str, output_csv: str) -> bool:
     print(f"Converting {input_csv}")
 
     with open(input_csv) as csv_in_file:
         reader = csv.DictReader(csv_in_file)
         clean_headers = clean_column_names(reader.fieldnames)
-        
-        with open(output_csv,'w') as csv_out_file:
-            writer = csv.DictWriter(csv_out_file,fieldnames=reader.fieldnames)
+        with open(output_csv, "w", newline="") as csv_out_file:
+            writer = csv.DictWriter(csv_out_file, fieldnames=clean_headers)
             writer.writeheader()
             for row in reader:
-                writer.writerow(row)
-        
-        with open(output_csv, "r+") as fix_out:
-            writer = csv.DictWriter(
-                fix_out,
-                fieldnames=clean_headers
-                )
-    
-            writer.writeheader()
+                clean_row = {
+                    clean_headers[i]: row[reader.fieldnames[i]]
+                    for i in range(len(reader.fieldnames))
+                }
+                writer.writerow(clean_row)
     return True
 
 
